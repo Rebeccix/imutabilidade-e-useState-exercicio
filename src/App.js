081b59b79ffde72936d23412d8e93bf753ccaf5b
Rebecca Ferreira <beccixzin@gmail.com>
2022-10-25 16:47:09 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,41 +1,96 @@
-import React, {useState} from 'react'
-import styled, {createGlobalStyle} from 'styled-components'
-import PokemonCard from './Components/PokemonCard/PokemonCard';
+import React, { useState } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import PokemonCard from "./Components/PokemonCard/PokemonCard";
 
+// ----------------------------------
 const GlobalStyles = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`
+`;
 const FlexContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-`
+`;
+// ----------------------------------
+
 function App() {
   const [pokemon, setPokemon] = useState({
     name: "Pichu",
     type: "Electric",
     evolved: false,
     weight: 2,
-    color: 'yellow',
-    image: 'https://archives.bulbagarden.net/media/upload/thumb/b/b9/172Pichu.png/250px-172Pichu.png',
-    id: 0
-  })
+    color: "yellow",
+    image:
+      "https://archives.bulbagarden.net/media/upload/thumb/b/b9/172Pichu.png/250px-172Pichu.png",
+    id: 0,
+  });
+
+  const [pokemon1, setPokemon1] = useState({
+    name: "Ralts",
+    type: "Psychic, Fairy",
+    evolved: false,
+    weight: 6,
+    color: "pink",
+    image:
+      "https://archives.bulbagarden.net/media/upload/thumb/e/e1/280Ralts.png/250px-280Ralts.png",
+    id: 0,
+  });
+
+  const [pokemon2, setPokemon2] = useState({
+    name: "Murkrow",
+    type: "Dark",
+    evolved: false,
+    weight: 4,
+    color: "gray",
+    image:
+      "https://archives.bulbagarden.net/media/upload/thumb/3/33/198Murkrow.png/250px-198Murkrow.png",
+    id: 0,
+  });
 
   // Para fazer seus próximos pokemons, crie novos estados!
+  // const a = [{name: 1}, {name: 2}, {name: 3}];
 
-  return ( <>
-  <GlobalStyles/>
-    <FlexContainer>
-      {/* Aqui neste componente, passe as props. Lembre-se que também é possivel passar a função de setState via props! */}
-      <PokemonCard/>
-      {/* Crie aqui seus próximos pokemons! */}
-    </FlexContainer>
-  </>
-    
+  // const list = a.map((index) => 
+  //   <p>{index.name}</p>
+  // );
+
+  return (
+    <>
+      <GlobalStyles />
+      <FlexContainer>
+        {/* Aqui neste componente, passe as props. Lembre-se que também é possivel passar a função de setState via props! */}
+        <PokemonCard pokemon={pokemon} setPokemon={setPokemon} />
+        <PokemonCard pokemon={pokemon1} setPokemon={setPokemon1} />
+        <PokemonCard pokemon={pokemon2} setPokemon={setPokemon2} />
+        {/* Crie aqui seus próximos pokemons! */}
+      </FlexContainer>
+    </>
   );
 }
 
 export default App;
+
+// const [pokemon, setPokemon] = useState([{
+//     name: "Pichu",
+//     type: "Electric",
+//     evolved: false,
+//     weight: 2,
+//     color: "yellow",
+//     image:
+//       "https://archives.bulbagarden.net/media/upload/thumb/b/b9/172Pichu.png/250px-172Pichu.png",
+//     id: 0,
+//   },
+//   {
+//     name: "Ralts",
+//     type: "Psychic, Fairy",
+//     evolved: false,
+//     weight: 6,
+//     color: "pink",
+//     image:
+//       "https://archives.bulbagarden.net/media/upload/thumb/e/e1/280Ralts.png/250px-280Ralts.png",
+//     id: 0
+//   }]
+// );
--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -1,24 +1,52 @@
-import React from 'react'
-import {Card, PokemonName, PokemonType, EvolveButton} from './styles'
-
-
+import { Card, PokemonName, PokemonType, EvolveButton } from "./styles";
+import React from "react";
 
 const PokemonCard = (props) => {
+  const { image, name, type, weight, color } = props.pokemon;
 
-    const evoluirPokemon = () => {
-        console.log("Cliquei no botão de evoluir")
+  const evoluirPokemon = () => {
+    console.log("Cliquei no botão de evoluir");
+
+    if (props.pokemon.name === "Pichu") {
+      props.setPokemon({
+        ...props.pokemon,
+        name: "Pikachu",
+        weight: 6,
+        evolved: true,
+        image:
+          "https://archives.bulbagarden.net/media/upload/thumb/0/0d/025Pikachu.png/250px-025Pikachu.png",
+      });
+    } else if (props.pokemon.name === "Ralts") {
+      props.setPokemon({
+        ...props.pokemon,
+        name: "Kirlia",
+        weight: 8,
+        evolved: true,
+        image:
+          "https://archives.bulbagarden.net/media/upload/thumb/0/00/281Kirlia.png/250px-281Kirlia.png",
+      });
+    } else if (props.pokemon.name === "Murkrow") {
+      props.setPokemon({
+        ...props.pokemon,
+        name: "Honchkrow",
+        weight: 14,
+        evolved: true,
+        image:
+          "https://archives.bulbagarden.net/media/upload/thumb/4/46/430Honchkrow.png/250px-430Honchkrow.png",
+      });
     }
-    
+  };
+
   return (
-    <Card color={}>
-        <img src={} alt={`Pokemon`}/>
-        <PokemonName>{}</PokemonName>
-        <PokemonType>{}</PokemonType>
-        <p>{}kg</p>
+    <Card color={color}>
+      <img src={image} alt={`Pokemon`} />
+      <PokemonName>{name}</PokemonName>
+      <PokemonType>{type}</PokemonType>
+      <p>{weight}kg</p>
 
-        <EvolveButton onClick={() => evoluirPokemon()}>Evoluir!</EvolveButton>
+      <EvolveButton onClick={() => evoluirPokemon()}>Evoluir!</EvolveButton>
     </Card>
-  )
-}
+  );
+};
 
-export default PokemonCard
+export default PokemonCard;
